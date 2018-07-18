86cbfe1a7a4e2d6baf16980f6269e96b0a8ec955
Jordan Endicott <jordan.endicott823@gmail.com>
2018-07-18 14:52:12 -0400

    .

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
 get '/' do
-  "hello world"
+  erb :pg1
+end
+
+post '/name' do
+  puts params
+  first_name = params[:first_name]
+  last_name = params[:last_name]
+  redirect '/pg2?first_name=' + first_name + '&last_name=' + last_name
+end
+
+get '/pg2' do
+  first_name = params[:first_name]
+  last_name = params[:last_name]
+  puts "in pg2 params is #{params}"
+  erb :pg2, locals: {first_name: first_name, last_name: last_name}
 end
