f94b055631e7a8327e9b1b0d2b63d7dad81603b8
Jordan Endicott <jordan.endicott823@gmail.com>
2018-07-18 15:42:42 -0400

    finished page 2 address page

--- app.rb
+++ app.rb
@@ -17,3 +17,19 @@ get '/pg2' do
   puts "in pg2 params is #{params}"
   erb :pg2, locals: {first_name: first_name, last_name: last_name}
 end
+
+post '/address' do
+  puts "#{params} is params in post adress"
+  first_name = params[:first_name]
+  last_name = params[:last_name]
+  street = params[:street]
+  city = params[:city]
+  state = params[:state]
+  zip = params[:zip]
+  redirect '/favnums?first_name=' + first_name + '&last_name=' + last_name + '&street=' + street + '&city=' + city + '&state=' + state + '&zip=' + zip
+end
+
+get '/favnums' do
+  puts "#{params} fav nums is"
+  
+end
